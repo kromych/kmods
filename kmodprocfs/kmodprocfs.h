cc39da8997662e313bfd6616ab791c34d467a776
kromych <kromych@users.noreply.github.com>
2020-09-20 22:05:27 -0700

    Using ioctl.h

--- kmodprocfs/kmodprocfs.h
+++ kmodprocfs/kmodprocfs.h
@@ -1,13 +1,15 @@
 #ifndef __kmod_procfs__
 #define __kmod_procfs__
 
+#include <linux/ioctl.h>
+
 #define PROC_DIR_NAME        "kmodprocfs"
 #define PROC_DIR_PATH        "/proc/" PROC_DIR_NAME "/"
 #define PROC_BUF_SIZE        4096
 #define PROC_MAX_NAME_LEN    32
 #define PROC_DEFAULT_ENTRIES 8
 
-#define PROC_IOCTL_BASE         ('P' << 24 | 'I' << 16 | 'R' << 8)
-#define PROC_IOCTL_RESET_POS    PROC_IOCTL_BASE | 0
+#define PROC_IOCTL_BASE         'P'
+#define PROC_IOCTL_RESET_POS    _IO(PROC_IOCTL_BASE, 0)
 
 #endif
