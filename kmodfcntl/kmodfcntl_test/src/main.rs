b946cf8e4aa295a469138a0a8746f5beb4ceb6e8
kromych <kromych@users.noreply.github.com>
2023-05-10 01:08:45 -0500

    User mode test

---
+++ kmodfcntl/kmodfcntl_test/src/main.rs
@@ -0,0 +1,28 @@
+use nix::fcntl::FcntlArg::F_SETFL;
+use nix::fcntl::OFlag;
+use std::io::Read;
+use std::os::fd::AsRawFd;
+
+fn main() {
+    let mut f = std::fs::File::open("/dev/kmod_fcntl").unwrap();
+    let mut buf = [0_u8; 1];
+
+    for i in 0..5 {
+        println!("--> Blocking read started #{i}");
+
+        let r = f.read_exact(&mut buf);
+        println!("<-- Blocking read finished #{i}, result {r:?}\n");
+    }
+
+    // Need to OR the flag?
+    nix::fcntl::fcntl(f.as_raw_fd(), F_SETFL(OFlag::O_NONBLOCK)).unwrap();
+
+    for i in 0..5 {
+        println!("--> Non-blocking read started #{i}");
+
+        let r = f.read_exact(&mut buf);
+        println!("<-- Non-blocking read finished #{i}, result {r:?}\n");
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+}
